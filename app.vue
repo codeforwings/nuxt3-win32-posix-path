bb83f56f0506b13ad2867945686d26781124ff3e
JC <jchan48@gmail.com>
2023-01-06 23:34:30 -0500

    added basic vue component without any styling yet

--- app.vue
+++ app.vue
@@ -1,5 +1,46 @@
 <template>
   <div>
-    <NuxtWelcome />
+<!--    <NuxtWelcome />-->
+    <h1>Win32 To Posix</h1>
+    <textarea
+      v-model="sInputWin32"
+    />
+    <!--    <NuxtWelcome />-->
+    <textarea
+        :value="sOutputWin32ToPosix"
+    />
+    <br/>
+    <h1>Posix to Win32</h1>
+    <textarea
+        v-model="sInputPosix"
+    />
+    <!--    <NuxtWelcome />-->
+    <textarea
+        :value="sOutputPosixToWin32"
+    />
   </div>
 </template>
+<script>
+export default {
+  name:'app',
+  data() {
+    return {
+      sInputWin32: "value",
+      sInputPosix: "value",
+
+    }
+  },
+  computed: {
+    sOutputWin32ToPosix() {
+
+      return this.sInputWin32.split('\n').map( val =>{
+
+        return val;
+      }).join('\n')
+    },
+    sOutputPosixToWin32() {
+      return this.sInputPosix
+    },
+  },
+}
+</script>
