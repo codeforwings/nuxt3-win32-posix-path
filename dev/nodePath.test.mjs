31ba6e277c622cf502cacd804505a5d13fcb8e11
JC <jchan48@gmail.com>
2023-01-06 21:45:37 -0500

    first init

---
+++ dev/nodePath.test.mjs
@@ -0,0 +1,93 @@
+/**
+yarn add mocha -D
+
+package.json
+  "imports": {
+    "##/*": {
+      "default": "./*"
+    },
+  },
+  "type": "module",
+
+  jsconfig.json
+  {
+  "compilerOptions": {
+    "baseUrl": ".",
+    "paths": {
+      "##/*": ["./*"]
+    }
+  },
+  "exclude": ["node_modules", ".nuxt", "dist"]
+}
+
+
+
+*/
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+// const assert = require('assert');
+// const {describe,it} = require('mocha');
+import assert from 'node:assert';
+import { describe, it} from 'mocha';
+/*
+1.
+yarn add mocha @babel/polyfill @babel/register @babel/preset-env babel-plugin-module-resolver --dev
+yarn add @babel/core --dev
+2.
+-r @babel/register -r babel-plugin-module-resolver
+
+3.
+.babelrc
+{
+
+  "presets": ["@babel/preset-env"],
+  "plugins": [
+    ["module-resolver", {
+      "root": ["./src"],
+      "alias": {
+        "test": "./test",
+        "underscore": "lodash",
+
+        "~": "./"
+      }
+    }]
+  ]
+
+}
+test specific timeout
+this.timeout(500);//500ms
+*/
+/**
+ * Should put this somewhere safe
+ * todo filepath needs to be initialized as well...
+ * @param fileName .json
+ * @param data will automatically be changed
+ */
+import fs from 'node:fs';
+function writeToFile(fileName,data,space=2){
+  const sFileName = /\./.test(fileName) ? fileName : fileName + '.json';
+  const filePath = `dev/pbs/test/${sFileName}`
+  fs.writeFileSync(filePath,
+    typeof data === 'string' ? data :JSON.stringify(data,null,+space)
+  );
+}
+
+/**
+ * Test Class "C"
+ */
+function C(input,expected) {
+  return {input,expected}
+}
+import {posix,win32} from 'node:path'
+describe('nodePath.test.mjs', function(){
+  const tests = [
+    C(
+      String.raw`C:\Users\Administrator\OneDrive\Documents\2022\someMD.md`,
+    )
+  ];
+  it('Path tests from windows', function(){
+    //assert.strictEqual(1,1);//require assert
+    console.log(posix.sep);
+
+  });
+});
