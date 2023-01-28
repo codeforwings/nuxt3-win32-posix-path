a509598d662c8d3d4d856ddff56102368e3e4d09
JC <jchan48@gmail.com>
2023-01-28 11:15:05 -0500

    tried adding vue through cdn. didnt work

--- app.vue
+++ app.vue
@@ -1,51 +1,50 @@
 <template>
-  <div>
-    <!--    sOutputWin32ToPosix />-->
-    <h1>Win32 To Posix</h1>
-    <textarea
-      class="area"
-      v-model="sInputWin32"
-    />
+  <div id="app">
+      <!--    sOutputWin32ToPosix />-->
+      <h1>Win32 To Posix</h1>
+      <textarea
+        class="area"
+        v-model="sInputWin32"
+      />
 
-    <textarea      class="area"
-        :value="sOutputWin32ToPosix"
-    />
-    <br/>
-    <!--    sOutputPosixToWin32 />-->
-    <h1>Posix to Win32</h1>
-    <textarea      class="area"
-        v-model="sInputPosix"
-    />
+      <textarea      class="area"
+          :value="sOutputWin32ToPosix"
+      />
+      <br/>
+      <!--    sOutputPosixToWin32 />-->
+      <h1>Posix to Win32</h1>
+      <textarea      class="area"
+          v-model="sInputPosix"
+      />
 
-    <textarea      class="area"
-        :value="sOutputPosixToWin32"
-    />
+      <textarea      class="area"
+          :value="sOutputPosixToWin32"
+      />
 
-    <!--    Win32 Normalized />-->
-    <h1>Win32 to Win32JS mnt</h1>
-    <textarea class="area"
-      v-model="sInputWin32Normal"
-    />
-    <textarea class="area"
-      :value="sOutputWin32Normal"
-    />
+      <!--    Win32 Normalized />-->
+      <h1>Win32 to Win32JS mnt</h1>
+      <textarea class="area"
+        v-model="sInputWin32Normal"
+      />
+      <textarea class="area"
+        :value="sOutputWin32Normal"
+      />
   </div>
 </template>
 <script>
 import {pathPosixToWin32, pathWin32ToPosix} from "~/pathReplacement.mjs";
 import {posixTests, win32Tests} from "~/dev/nodePathTestExamples.mjs";
 import {win32ToWin32JS} from "~/dev/win32ToWin32JS.mjs";
-
 export default {
   name:'app',
+  mounted(){
+
+  },
   data() {
     return {
       sInputWin32: win32Tests.map(val => val.input).join('\n'),
       sInputPosix: posixTests.map(val => val.input).join('\n'),
       sInputWin32Normal:"C:\\Users\\Jason\\OneDrive\\Documents\\2022\\someMD.md",
-
-
-
     }
   },
   computed: {
