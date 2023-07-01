1d34b929695e7863482214e0d13818432c206ec3
jasonchan <jchan48@gmail.com>
2023-07-01 00:23:29 -0400

    updated mac and linux runners

--- src/cli/cli-index.test.mjs
+++ src/cli/cli-index.test.mjs
@@ -100,10 +100,14 @@ describe('cli-index.test.mjs', function(){
     });
 
 });
+/**
+ * Didn't even need the index-linux... because it worked with the exe
+ */
 describe('cli-index.test.mjs binary win', function(){
     // const binaryPath= "lib/dist/bins"
     const binaryCommand = '"lib/dist/bins/index-win.exe"'
     const binaryCommandWSL = './lib/dist/bins/index-linux'
+    // const binaryCommandWSL = './lib/dist/bins/index-win.exe';//also worked...
     if(os.platform()    !== 'win32'){
         return true;
     }
@@ -120,10 +124,49 @@ describe('cli-index.test.mjs binary win', function(){
         assert.strictEqual(stdout,expected)
 
     });
-        it('verify command line wsl', function(){
+    it('verify command line wsl', function(){
         //assert.strictEqual(1,1);//require assert
         const expected = "/mnt/c/Users/Public/bins\n"
         const out = spawnSync('wsl -e',[binaryCommandWSL,"C:\\Users\\Public\\bins"],{shell:true})
+        const stdout = out.stdout.toString();
+        if(out.status !==0){
+            console.error(out.stderr.toString())
+            throw out.error;
+        }
+        assert.strictEqual(stdout,expected)
+
+    });
+    it('verify command line mjs error', function(){
+        //assert.strictEqual(1,1);//require assert
+        const out = spawnSync(binaryCommand,[],{shell:true})
+        // console.log(out.stdout.toString());
+        if(out.status !==0){
+            assert.ok(true)
+            // console.error(out.stderr.toString())
+            // throw out.error;
+        }
+    });
+
+});
+
+
+/**
+ * Don't need this binary. just testing it
+ * interestingly... it failed
+ */
+describe('cli-index.test.mjs binary mac', function(){
+    // const binaryPath= "lib/dist/bins"
+    let binaryCommand = './lib/dist/bins/index-macos';//
+    // binaryCommand = 'lib/dist/bins/index-macos' //this worked too
+    binaryCommand = './lib/dist/bins/index-macos' //interesting
+    if(os.platform() !== 'darwin'){
+        return true;
+    }
+
+    it('verify command line mjs', function(){
+        //assert.strictEqual(1,1);//require assert
+        const expected = "/mnt/c/Users/Public/bins\n"
+        const out = spawnSync(binaryCommand,["C:\\Users\\Public\\bins"],{shell:true})
         const stdout = out.stdout.toString();
         if(out.status !==0){
             console.error(out.stderr.toString())
