5b6d90084112e1db67e573a6695a51b09dbbc3ef
jasonchan <jchan48@gmail.com>
2023-07-04 12:27:18 -0400

    updated test scripts and generated the parsed.txt file

---
+++ dev/windows_env_paths/cli-escape-lines.md
@@ -0,0 +1,5 @@
+# Cli Escape Lines
+```bash
+node ./cli-node.mjs "c:/Users/Public/Docum ents"
+node ./cli-node.mjs "c:/Users/Public/Documents"
+```
---
+++ dev/windows_env_paths/cli-node.mjs
@@ -0,0 +1,7 @@
+/**
+ * Testing how zsh passes arguments to node
+ node dev/windows_env_paths/cli-node.mjs "c:/Users/Public/Docum ents"
+ * @type {string[]}
+ */
+const args = process.argv.slice(2);
+console.log(args,'len: ',args.length);
