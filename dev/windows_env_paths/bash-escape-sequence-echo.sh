67d4c784afd065c48ec301a0a3133c539994956e
JC <jchan48@gmail.com>
2023-07-04 02:13:14 -0400

    updated notes on escaping in sh

--- dev/windows_env_paths/bash-escape-sequence-echo.sh
+++ dev/windows_env_paths/bash-escape-sequence-echo.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env fish
+# can't end with \' or \"
 # echo 'c\'
 # echo "c\"
-# echo c\
+ echo c\
+
 # work
 echo 'c\hi'
 echo "c\hi"
@@ -11,3 +13,16 @@ echo c\\hi
 # no work:
 echo c\hi
 
+
+# Home
+echo ~
+echo ~/repo
+## quotes dont work at all with echo at least
+echo "~"
+echo '~'
+echo "~/repo"
+echo '~/repo'
+echo "$HOME" # works
+echo '$HOME'
+echo "$HOME/repo" # works
+echo '$HOME/repo'
