b7d60903ada4297ec5f283b30cbf03c6f4fcb615
JC <jchan48@gmail.com>
2023-07-18 14:51:37 -0400

    cygwin works... but broke the push to gh

--- nuxt.config.mjs
+++ nuxt.config.mjs
@@ -5,7 +5,7 @@ process.env.NUXT_APP_BASE_URL = '/nuxt3-win32-posix-path/'
 const baseURL = process.env.NUXT_APP_BASE_URL || '/nuxt3-win32-posix-path/'
 export default defineNuxtConfig({
   ssr:false,
-  baseURL,
+  // baseURL,//this breaks it? wth
   // env: {
   //   NUXT_APP_BASE_URL: process.env.NUXT_APP_BASE_URL || 'http://localhost:3000',
   // }
